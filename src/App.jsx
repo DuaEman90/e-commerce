25edd9568164b428b9472bd3d905578142b3b9d4
Dua Eman <d27065204@gmail.com>
2026-09-02 12:57:42 +0500

    feat: update routing structure and integrate page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
+import RootLayout from "./layout/RootLayout";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
     <div>
-        <Routes>
-            <Route path="/" element={<RootLayout />} >
-            <Route index element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-           </Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
